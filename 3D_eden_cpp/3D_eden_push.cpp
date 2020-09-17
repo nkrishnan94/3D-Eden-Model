15a3beacb5db8953f0e9456869c6db5d5d89196b
Nikhil Krishnan <npk13@case.edu>
2020-09-16 20:10:56 -0400

    "pushed" model working

--- 3D_eden_cpp/3D_eden_push.cpp
+++ 3D_eden_cpp/3D_eden_push.cpp
@@ -39,7 +39,7 @@ int main(){
 	struct tm * timeinfo;
 	char buffer [80];
 	float ht = 0.5;
-	int prof_count = 4;
+	int prof_count = 1;
 
 
 	time (&time_start);
@@ -61,42 +61,46 @@ int main(){
 	}
 
 
-
-	int neighb[6][3] = {{-1,0,0},{1,0,0},{0,-1,0},{0,1,0},{0,0,-1},{0,0,1}};
 	vector <int> fullx;
 	vector <int> fully;
 	vector <int> fullz;
-	vector <int> nottrappedx;
-	vector <int> nottrappedy;
-	vector <int> nottrappedz;
-
-	for (int dt = 0 ; dt < n_gens; dt++ ){		
-		vector <int> nottrappedx;
-		vector <int> nottrappedy;
-		vector <int> nottrappedz;
 
 
-		
+	for (int dt = 0 ; dt < n_gens; dt++ ){	
+		ostringstream strT;
+		strT << dt;
+		cout <<	dt <<endl;
+
+		//cout <<	dt <<endl;
+
 		for(int i = 0; i < xdemes; i++){
+		
+
 			for(int j = 0; j < ydemes; j++){
+			
 				for(int k = 0; k < zdemes; k++){
-					if (deme[i][j][k] ==1){
-						
-						//fullx.push_back(i);
-						//fully.push_back(j);
-						//fullz.push_back(k);
+					if (deme[i][j][k]==1){
+
+						/*if ((count(fullx.begin(),fullx.end(),i)) ==0 && (count(fully.begin(),fully.end(),j)) ==0  &&(count(fullz.begin(),fullz.end(),k)) ==0 ){
+
+
+							fullx.push_back(i);
+							fully.push_back(j);
+							fullz.push_back(k);
+						}*/	
 
 						int pick = rand() % 6 + 0;
 
-
-
 						if(pick==0){
-							int empty_flag = 0;
-							for(int x =i-1; x>0; x--){
-								if ((deme[x][j][k] ==0) &&  (empty_flag==0)){
 
-									deme[x][j][k] =1;
-									empty_flag = 1;
+							int empty_flag = 0;
+							if(i>0){
+								for(int x =i-1; x>0; x--){
+									if ((deme[x][j][k] ==0) &&  (empty_flag==0)){
+
+										deme[x][j][k] =1;
+										empty_flag = 1;
+									}
 								}
 							}
 
@@ -104,11 +108,13 @@ int main(){
 
 						if(pick==1){
 							int empty_flag = 0;
-							for(int x =i+1; x<xdemes; x++){
-								if ((deme[x][j][k] ==0) &&  (empty_flag==0)){
-									deme[x][j][k] =1;
-									empty_flag = 1;
-									
+							if (i<xdemes-1){
+								for(int x =i+1; x<xdemes; x++){
+									if ((deme[x][j][k] ==0) &&  (empty_flag==0)){
+										deme[x][j][k] =1;
+										empty_flag = 1;
+										
+									}
 								}
 							}
 
@@ -118,11 +124,13 @@ int main(){
 
 						if(pick==2){
 							int empty_flag = 0;
-							for(int y = j-1; y>0; y--){
-								if ((deme[i][y][k] ==0) &&  (empty_flag==0)){
+							if (j>0){
+								for(int y = j-1; y>0; y--){
+									if ((deme[i][y][k] ==0) &&  (empty_flag==0)){
 
-									deme[i][y][k] =1;
-									empty_flag = 1;
+										deme[i][y][k] =1;
+										empty_flag = 1;
+									}
 								}
 							}
 
@@ -130,11 +138,13 @@ int main(){
 
 						if(pick==3){
 							int empty_flag = 0;
-							for(int y =j+1; y<ydemes; y++){
-								if ((deme[i][y][k] ==0) &&  (empty_flag==0)){
-									deme[i][y][k] =1;
-									empty_flag = 1;
-									
+							if (j<ydemes-1){
+								for(int y =j+1; y<ydemes; y++){
+									if ((deme[i][y][k] ==0) &&  (empty_flag==0)){
+										deme[i][y][k] =1;
+										empty_flag = 1;
+										
+									}
 								}
 							}
 
@@ -143,36 +153,145 @@ int main(){
 
 						if(pick==4){
 							int empty_flag = 0;
-							for(int z =k-1; z>0; z--){
-								if ((deme[i][j][z] ==0) &&  (empty_flag==0)){
+							if (k>0){
+								for(int z =k-1; z>0; z--){
+									if ((deme[i][j][z] ==0) &&  (empty_flag==0)){
 
-									deme[i][j][z] =1;
-									empty_flag = 1;
+										deme[i][j][z] =1;
+										empty_flag = 1;
+									}
 								}
 							}
 						}
 
 						if(pick==5){
 							int empty_flag = 0;
-							for(int z =k+1; z<zdemes; k++){
-								if ((deme[i][j][z] ==0) &&  (empty_flag==0)){
-									deme[i][j][z] =1;
-									empty_flag = 1;
-									
+							if(k<zdemes-1){
+								for(int z =k+1; z<zdemes; z++){
+									if ((deme[i][j][z] ==0) &&  (empty_flag==0)){
+										deme[i][j][z] =1;
+										empty_flag = 1;
+										
+									}
 								}
 							}
 
 						}
 
-						
-
-
 					}
-
 
 				}
 			}
 		}
+
+	
+
+
+		//int pick_deme = rand() % fullx.size() + 0;
+		//cout <<pick_deme<<endl;
+		//cout <<"hi"<<endl;
+
+		
+		//fullx.push_back(i);
+		//fully.push_back(j);
+		//fullz.push_back(k);
+
+		/*int pick = rand() % 6 + 0;
+		int i =fullx[pick_deme];
+		int j =fully[pick_deme];
+		int k =fullz[pick_deme];
+
+
+
+		if(pick==0){
+			int empty_flag = 0;
+			if(i>0){
+				for(int x =i-1; x>0; x--){
+					if ((deme[x][j][k] ==0) &&  (empty_flag==0)){
+
+						deme[x][j][k] =1;
+						empty_flag = 1;
+					}
+				}
+			}
+
+		}
+
+		if(pick==1){
+			int empty_flag = 0;
+			if (i<xdemes-1){
+				for(int x =i+1; x<xdemes; x++){
+					if ((deme[x][j][k] ==0) &&  (empty_flag==0)){
+						deme[x][j][k] =1;
+						empty_flag = 1;
+						
+					}
+				}
+			}
+
+		}
+
+
+
+		if(pick==2){
+			int empty_flag = 0;
+			if (j>0){
+				for(int y = j-1; y>0; y--){
+					if ((deme[i][y][k] ==0) &&  (empty_flag==0)){
+
+						deme[i][y][k] =1;
+						empty_flag = 1;
+					}
+				}
+			}
+
+		}
+
+		if(pick==3){
+			int empty_flag = 0;
+			if (j<ydemes-1){
+				for(int y =j+1; y<ydemes; y++){
+					if ((deme[i][y][k] ==0) &&  (empty_flag==0)){
+						deme[i][y][k] =1;
+						empty_flag = 1;
+						
+					}
+				}
+			}
+
+		}
+
+
+		if(pick==4){
+			int empty_flag = 0;
+			if (k>0){
+				for(int z =k-1; z>0; z--){
+					if ((deme[i][j][z] ==0) &&  (empty_flag==0)){
+
+						deme[i][j][z] =1;
+						empty_flag = 1;
+					}
+				}
+			}
+		}
+
+		if(pick==5){
+			int empty_flag = 0;
+			if(k<zdemes-1){
+				for(int z =k+1; z<zdemes; z++){
+					if ((deme[i][j][z] ==0) &&  (empty_flag==0)){
+						deme[i][j][z] =1;
+						empty_flag = 1;
+						
+					}
+				}
+			}
+
+		}*/
+
+		
+
+
 
 
 
@@ -191,7 +310,7 @@ int main(){
             	for(int j = 0; j <ydemes; j++){
             		for(int k = 0; k <zdemes; k++){
             			if (deme[i][j][k]==1){
-            				fproft << i << ", " << j << ", "<< k<< ", " <<endl;
+            				fproft << i << ", " << j << ", "<< k <<endl;
 
             			}
 
@@ -199,6 +318,7 @@ int main(){
     				}
             	}
         	}
+        	
 
         	//fproft.close(proftName);
 
